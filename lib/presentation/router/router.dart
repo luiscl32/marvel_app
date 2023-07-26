7075cac8e74d6ce8cf02a3a79e76f37d8474f083
Luis Correa <luiscl32@gmail.com>
2023-07-26 15:57:01 -0400

    feat: :art: adding router

--- lib/presentation/router/router.dart
+++ lib/presentation/router/router.dart
@@ -1,1 +1,16 @@
-class ScreenPaths {}
+import 'package:flutter/material.dart';
+import 'package:marvel_app/presentation/screens/screens.dart';
+
+class ScreenPaths {
+  ScreenPaths._();
+  static const String dashboard = '/dashboard';
+  static const String detail = '/detail';
+}
+
+class CustomRouter {
+  CustomRouter._();
+  static Map<String, WidgetBuilder> routes = {
+    ScreenPaths.dashboard: (context) => const DashboardPage(),
+    ScreenPaths.detail: (context) => const DetailPage(),
+  };
+}
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:marvel_app/presentation/screens/dashboard/dashboard.page.dart';
+export 'package:marvel_app/presentation/screens/detail/detail.page.dart';

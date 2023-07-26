4de5dc036265c233eb361900243a551f840ad12a
Luis Correa <luiscl32@gmail.com>
2023-07-26 16:53:39 -0400

    feat: :art: barrel file widget and add api config file

---
+++ lib/data/api/api.config.dart
@@ -0,0 +1,14 @@
+class _ApiKeys {
+  _ApiKeys._();
+
+  static const publicKey = 'd298f8f70506d4b5cac15d7274e46c60';
+  static const hash = 'dab9da409a376999f7768b9e94a60b04';
+}
+
+class ApiConfig {
+  ApiConfig._();
+
+  String getRequest(String endpoint) {
+    return 'http://gateway.marvel.com/v1/public/$endpoint?ts=1000&apikey=${_ApiKeys.publicKey}&hash=${_ApiKeys.hash}';
+  }
+}
